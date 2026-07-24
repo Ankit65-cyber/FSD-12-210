4b2819a377215a7ba08c72afdd5a046306d895de
ankit <ank71887@gmail.com>
2026-07-24 14:51:36 +0530

    commit

--- lab1/eventEmitter.js
+++ lab1/eventEmitter.js
@@ -1,15 +1,25 @@
-import { EventEmitter } from "nodes:events";
-const login=(name)=> {
+import { EventEmitter } from "node:events";
+
+const login = (name) => {
   console.log(`${name} logged in`);
 };
 
-const start=()=>{
+const start = () => {
   console.log("system starts");
 };
-const working=(name) => {
+
+const working = (name) => {
   console.log(`${name} add items to cart`);
 };
-const checkout=(name)=>{
+
+const checkout = (name) => {
   console.log(`${name} logged out`);
-}
+};
+
 login("ankit yadav");
+
+const task = new EventEmitter();
+
+task.on("greet", login);
+
+task.emit("greet", "ankit yadav");
