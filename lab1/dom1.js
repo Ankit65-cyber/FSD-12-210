3da51b59a221e1ae8d60a6ba5a3bd7ff3b22af20
ankit <ank71887@gmail.com>
2026-07-30 11:02:42 +0530

    dom2

---
+++ lab1/dom1.js
@@ -0,0 +1,11 @@
+//dom->document object model
+import { EvenEmitter} from "events";
+
+const button = new EvenEmitter();
+ContentVisibilityAutoStateChangeEvent.on("click",() => {
+    console.log("button clicked";
+    });
+    button.emit("click");
+    
+
+
